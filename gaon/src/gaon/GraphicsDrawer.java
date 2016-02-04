5767bf15c3d4c47b25603786d219bc251723ffdc
d4c <hyyrolll@gmail.com>
2016-02-05 02:37:55 +0900

    mod

--- gaon/src/gaon/GraphicsDrawer.java
+++ gaon/src/gaon/GraphicsDrawer.java
@@ -1,20 +1,21 @@
 package gaon;
-import java.applet.Applet;
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Image;
 
-public class GraphicsDrawer extends Applet{
+public class GraphicsDrawer extends Component {
 	private static final long serialVersionUID = 7341738986888373924L;
 
 	private Dimension size;
 	private Image back;
 	private Graphics buffer;
-	private Applet applet;
+	private Component comp;
 
-	public <T extends Applet> GraphicsDrawer(T applet){
-		size = applet.getSize();
-		back = applet.createImage(size.width, size.height);
+	public <T extends Component> GraphicsDrawer(Component comp){
+		this.comp = comp;
+		size = comp.getSize();
+		back = comp.createImage(size.width, size.height);
 		buffer = back.getGraphics();
 	}
 
@@ -23,6 +24,6 @@ public class GraphicsDrawer extends Applet{
 	}
 
 	public void draw(Graphics g) {
-		g.drawImage(back, 0, 0, applet);
+		g.drawImage(back, 0, 0, comp);
 	}
 }
