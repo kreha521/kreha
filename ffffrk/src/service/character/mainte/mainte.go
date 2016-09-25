e79f69ee3c32b9f8c12ebfefd49e14f2e391ce68
Masaaki <Masaaki@d4clab-1890>
2016-09-26 02:25:31 +0900

    作業牛

--- ffffrk/src/service/character/mainte/mainte.go
+++ ffffrk/src/service/character/mainte/mainte.go
@@ -5,7 +5,12 @@ import (
 	"dao"
 	"dto"
 	"service/character/ref"
+	"github.com/jinzhu/gorm"
+	_ "github.com/lib/pq"
+	_ "github.com/go-sql-driver/mysql"
 )
+
+//https://github.com/go-sql-driver/mysql/issues/97
 
 func CreateCharacter(character dto.Character) (error, dto.Character) {
 	fmt.Println("kita")
@@ -17,24 +22,31 @@ func CreateCharacter(character dto.Character) (error, dto.Character) {
 
 	err, conn = dao.BeginTransaction(conn)
 	if (err != nil) {
-	fmt.Println("begin")
+		fmt.Println("begin")
 		return err, newCharacter
 	}
 
 	if err := conn.Table("characters").Create(&character).Error; err != nil {
-	fmt.Println("create")
+		fmt.Println("create")
 		conn.Rollback()
 		return err, newCharacter
 	}
 
-	if err, newCharacter = ref.GetCharacter("3"); err != nil {
+	var newId string
+	if err, newId = getLastInsertId(conn); err != nil {
+		fmt.Println("getlastid")
+		return err, newCharacter
+	}
+
+	if err, newCharacter = ref.GetCharacter(newId); err != nil {
+		fmt.Println("getlastdata:" + newId)
 		return err, newCharacter
 	}
 
 	conn.Commit();
 
 	if err := conn.Close(); err != nil {
-	fmt.Println("close")
+		fmt.Println("close")
 		return err, newCharacter
 	}
 	fmt.Println("owatta")
@@ -42,3 +54,17 @@ func CreateCharacter(character dto.Character) (error, dto.Character) {
 	return nil, newCharacter
 }
 
+func getLastInsertId(conn *gorm.DB) (error, string) {
+	rows, err := conn.Raw("select last_insert_id() AS last").Rows()
+	if (err != nil) {
+		return err, ""
+	}
+
+	defer rows.Close()
+
+	var last string
+	for rows.Next() {
+		rows.Scan(&last)
+	}
+	return nil, last
+}
