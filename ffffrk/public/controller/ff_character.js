b15f6ac7e814abf56b0657b25df80d29a15c654e
Masaaki <Masaaki@d4clab-1890>
2016-10-07 06:52:01 +0900

    バリデーター

--- ffffrk/public/controller/ff_character.js
+++ ffffrk/public/controller/ff_character.js
@@ -35,17 +35,54 @@ angular.module('ffffrk')
 	};
 })
 
-.directive('singleByteValidator',function(){
+.directive('singleByte',function(){
     return{
         restrict: "A",
         require: "ngModel",
         link: function(scope,element,attrs,ngModel){
             ngModel.$validators.singleByte = function(modelValue,viewValue){
                 var value = modelValue || viewValue;
-                return /^[\x20-\x7E]+$/.test(value);
+                if  (!value) {
+                	return true;
+                }
+                return /^[\x20-\x7E]*$/.test(value);
             }
         }
     }
 })
+
+.directive('number',function(){
+    return{
+        restrict: "A",
+        require: "ngModel",
+        link: function(scope,element,attrs,ngModel){
+            ngModel.$validators.number = function(modelValue,viewValue){
+                var value = modelValue || viewValue;
+                if  (!value) {
+                	return true;
+                }
+                return /^[0-9]*$/.test(value);
+            }
+        }
+    }
+})
+
+
+.directive('alphanumber',function(){
+    return{
+        restrict: "A",
+        require: "ngModel",
+        link: function(scope,element,attrs,ngModel){
+            ngModel.$validators.alphanumber = function(modelValue,viewValue){
+                var value = modelValue || viewValue;
+                if  (!value) {
+                	return true;
+                }
+                return /^[0-9a-zA-Z]*$/.test(value);
+            }
+        }
+    }
+})
+
 
 ;
