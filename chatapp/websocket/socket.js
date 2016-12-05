e6c5ef346f5de04d1aae9eb0cf2d31cb92ab4568
Masaaki <Masaaki@d4clab-1890>
2016-12-06 07:46:32 +0900

    urya

--- chatapp/websocket/socket.js
+++ chatapp/websocket/socket.js
@@ -7,54 +7,56 @@ var mysql = require('mysql');
 //SQL文を書く
 const sql = 'SELECT * FROM characters WHERE id = ?;';
 
-module.exports = function (socket) {
-    console.log("socket start");
-    
-    socket.on('send:time', function (data) {
-	      console.log(data.time);
-			socket.emit('send:time', {
-			      time: 'initial send'
+module.exports = {
+	onSocket: function (socket) {
+	    console.log("socket start");
+	    
+	    socket.on('send:time', function (data) {
+		      console.log(data.time);
+				socket.emit('send:time', {
+				      time: 'initial send'
+				    });
+	    });
+	
+	  setInterval(function () {
+	  	var connection = mysql.createConnection({
+			  host     : 'localhost', //接続先ホスト
+			  user     : 'root',      //ユーザー名
+			  password : '4you6so',  //パスワード
+			  database : 'testdatabase',    //DB名
+		      charset : 'utf8'
+			});
+	
+		//接続します
+		connection.connect();
+	
+		//プレースホルダー使ってSQL発行
+		var query = connection.query(sql, ['26']);
+	    var record;
+		//あとはイベント発生したらそれぞれよろしくねっ
+		query
+		  //エラー用
+		  .on('error', function(err) {
+		  })
+	
+		  //結果用
+		  .on('result', function(rows) {
+			console.log(rows.name);
+			if (rows.name != "Teeda") {
+				socket.emit('send:time', {
+			      time: rows.name
 			    });
-    });
-
-  setInterval(function () {
-  	var connection = mysql.createConnection({
-		  host     : 'localhost', //接続先ホスト
-		  user     : 'root',      //ユーザー名
-		  password : '4you6so',  //パスワード
-		  database : 'testdatabase',    //DB名
-	      charset : 'utf8'
-		});
-
-	//接続します
-	connection.connect();
-
-	//プレースホルダー使ってSQL発行
-	var query = connection.query(sql, ['26']);
-    var record;
-	//あとはイベント発生したらそれぞれよろしくねっ
-	query
-	  //エラー用
-	  .on('error', function(err) {
-	  })
-
-	  //結果用
-	  .on('result', function(rows) {
-		console.log(rows.name);
-		if (rows.name != "Teeda") {
-			socket.emit('send:time', {
-		      time: rows.name
-		    });
-		} else {
-			socket.emit('send:time', {
-		      time: "normal"
-		    });
-		}
-	  })
-
-	  //終わったよう～
-	  .on('end', function() {
-	    connection.destroy(); //終了
-	  });
-  }, 5000);
+			} else {
+				socket.emit('send:time', {
+			      time: "normal"
+			    });
+			}
+		  })
+	
+		  //終わったよう～
+		  .on('end', function() {
+		    connection.destroy(); //終了
+		  });
+	  }, 5000);
+	}
 };
